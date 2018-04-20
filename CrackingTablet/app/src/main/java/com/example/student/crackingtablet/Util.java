331afb681e5f602b8f451515dc9334e8043f2546
M140320\student <fksdud456@gmail.com>
2018-04-20 11:27:17 +0900

    Update Util class
    + Json function

--- CrackingTablet/app/src/main/java/com/example/student/crackingtablet/Util.java
+++ CrackingTablet/app/src/main/java/com/example/student/crackingtablet/Util.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Util {
     public static ArrayList<User> getListFromJSON(ArrayList<User> list, String josnString) {
@@ -16,6 +17,43 @@ public class Util {
                 // jo.toString("img")
                 int imgNum = 0;
                 list.add(new User(jo.getString("id"), "", jo.getString("name"), jo.getString("registdate"), imgNum, 0));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static ArrayList<String> getStringListFromJSON(ArrayList<String> list, String josnString) {
+        try {
+            JSONArray ja = new JSONArray(josnString);
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject jo = ja.getJSONObject(i);
+
+                // jo.toString("img")
+                int imgNum = 0;
+                list.add(jo.getString("id"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static HashMap<String, User> getListFromJSON(HashMap<String, User> list, String josnString) {
+        try {
+            JSONArray ja = new JSONArray(josnString);
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject jo = ja.getJSONObject(i);
+
+                // jo.toString("img")
+                int imgNum = 0;
+                list.put(jo.getString("id"),
+                        new User(jo.getString("id"),
+                                "",
+                                jo.getString("name"),
+                                jo.getString("registdate"),
+                                imgNum, 0));
             }
         } catch (JSONException e) {
             e.printStackTrace();
